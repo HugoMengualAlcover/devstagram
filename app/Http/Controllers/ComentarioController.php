f68eba959ebee101a5145d0f6519e1c1da1eea01
Sergio Marqués Muñoz <sergiomail.marques@gmail.com>
2023-03-22 12:52:59 +0100

    likes y tal

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Comentario;
 use Illuminate\Http\Request;
 
 class ComentarioController extends Controller
 {
-    //
+    public function store(Request $request, User $user, Post $post)//Este user no sirve porque es el creador del post, no el usuario 
+                                                                   //logeado que va a publicar el comentario
+    {
+        $this->validate($request, [
+            'comentario' => 'required|max:225'
+        ]);
+
+        Comentario::create([
+            'user_id' => auth()->user()->id,
+            'post_id' => $post->id,
+            'comentario' => $request->comentario
+        ]);
+
+        return back()->with('mensaje', 'Comentario Realizado Correctamente');
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,12 +5,38 @@
 @endsection
 
 @section('contenido')
-    <div class="container mx-auto flex">
-        <div class="md:w-1/2">
+    <div class="container mx-auto md:flex">
+        <div class="md:w-1/2 mx-5">
                 <img src="{{asset('uploads') . '/' . $post->imagen}}" alt="Imagen del post {{$post->titulo}}">
 
-                <div class="p-3">
-                    <p>Likes</p>
+                <div class="p-3 flex">
+                @auth
+                    @if( $post->checkLike(auth()->user() ))
+                        <form method="POST" action="{{ route('posts.likes.destroy', $post) }}">
+                            @method('DELETE')
+                            @csrf
+                            <div>
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                                    </svg>
+                                </button>
+                            </div>   
+                        </form>
+                    @else
+                        <form method="POST" action="{{ route('posts.likes.store', $post) }}">
+                            @csrf
+                            <div>
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                                    </svg>
+                                </button>
+                            </div>   
+                        </form>
+                    @endif
+                @endauth
+                    <p class="font-bold mx-1.5">{{ $post->likes->count() }}<span class="font-normal">Likes</span></p>
                 </div>
 
                 <div class="p-3">
@@ -18,22 +44,42 @@
                     <p class="text-sm text-gray-500"> {{$post->created_at->diffForHumans()}} </p>
                     <p class="mt-5"> {{$post->descripcion}} </p>
                 </div>
-        </div>
-
-        <div class="md:w-1/2">
-            <div class="shadow bg-white p-5 mb-5">
 
                 @auth
+                    @if ($post->user_id === auth()-> user()->id)
+                        <form method="GET" action="{{ route('posts.destroy', $post) }}">
+                            @method('DELETE')
+                            <input 
+                                type="submit"
+                                value="Eliminar Publicación"
+                                class="bg-red-500 hover:bg-red-600 p-2 rounded text-white font-bold mt-4 cursor-pointer"
+                            />
+                        </form>
+                    @endif
+                @endauth
+                
+        </div>
+
+        <div class="md:w-1/2 mx-5">
+            <div class="shadow bg-white p-5 mb-5">
                     
-                <p class="text-xl font-bold text-center mb-4">
-                    Agrega un Nuevo Comentario
+                <p class="text-xl font-bold text-center uppercase mb-4">
+                    Comentarios
                 </p>
-            </div>
+
+            @auth
+
+                @if (session('mensaje'))
+                    <div class="bg-green-500 p-2 rounded-lg mb-6 text-white text-center uppercase font-bold">
+                        {{session('mensaje')}}
+                    </div>
+                @endif
 
             <form action="{{ route('comentarios.store', ['post' => $post, 'user' => $user]) }}" method="POST">
+                @csrf
                 <div class="mb-5">
-                    <label for="decripcion" class="mb-2 block uppercase text-gray-500 font-bold" >
-                        Comentarios
+                    <label for="decripcion" class="mb-2 block text-gray-500 font-bold" >
+                        Agrega un Nuevo Comentario
                     </label>
                     <textarea
                         id="comentario"
@@ -49,12 +95,29 @@
 
                 <input 
                     type="submit"
-                    value="Crear Post"
+                    value="Comentar"
                     class="bg-sky-600 hover:bg-sky-800 transition-colors cursor-pointer
-                    uppercase font-bold w-full p-3 text-white rounded-lg"
+                    uppercase font-bold w-full p-3 text-white rounded-lg my-3"
                 />
             </form>
             @endauth
+
+            <div class="bg-white shadow mb-5 max-h-96 overflow-y-scroll ">
+                @if ($post->comentarios->count())
+                    @foreach ( $post->comentarios as $comentario )
+                        <div class="p-5 border-gray-300 border-b">
+                            <a href="{{ route('posts.index', $comentario->user) }}" class="font-bold">
+                                {{$comentario->user->username}}
+                            </a>
+                            <p>{{$comentario->comentario}}</p>
+                            <p>{{$comentario->created_at->diffForHumans()}}</p>
+                        </div>
+                    @endforeach
+                @else
+                    <p class="p-10 text-center">No hay comentarios aún</p>
+                @endif
+            </div>
+        </div>
         </div>
     </div>
 @endsection
